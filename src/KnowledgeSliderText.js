f62e25f5ea9dc9bbc7d895a9e783797e369d6778
mmsyg <mmsygula@gmail.com>
2023-11-02 11:13:43 +0100

    26X

--- src/KnowledgeSliderText.js
+++ src/KnowledgeSliderText.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import data from './assets/datta.json'
-import useScrollPosition from "./UseScrollPosition";
+import { useScrollWithShadow } from "./useScrollWithShadow";
 
 const KnowledgeSliderText = (props) => {
 
     const que = data.knowledgeSlider.filter(questionx => questionx.id === props.id);
-
-    const scrollPosition = useScrollPosition();
-
-console.log(scrollPosition); 
+    const { boxShadow, onScrollHandler } = useScrollWithShadow();
 
 
     return ( <div className="knowledge_slider_text">
         <p className="konwledge_title">{que[0].title}</p>
         <p className="konwledge_subtitle">{que[0].subtitle}</p>
-        <p className="konwledge_text">{que[0].text}</p>
+        <div className="scrollArea" onScroll={onScrollHandler}
+        style={{ boxShadow: boxShadow }}>
+      
+    {que[0].text}</div>
    
 
 
