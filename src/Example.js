d4153726c4886618315577fea4666a1fb61ec3a9
mmsyg <mmsygula@gmail.com>
2023-11-19 00:59:00 +0100

    18XIv2

--- src/Example.js
+++ src/Example.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
-import QuizzDescription from './QuizzDescription';
-import QuizzSummary from './QuizzSumary';
 import SetLeft from './SetLeft';
-
+import SetRight from './SetRight';
+import QuizzDescription from './QuizzDescription';
+import Carousel from './Carousel';
+import KnowledgeSlider from './KnowledgeSlider';
 
 const MySlider = () => {
+  const numberOfSlides = 2; // Określ liczbę slajdów, które chcesz wyrenderować
+
+  // Generowanie slajdów z różnymi id dla KnowledgeSlider
+  const slides = [];
+  for (let i = 1; i <= numberOfSlides; i++) {
+    slides.push(
+      <SwiperSlide key={i}>
+        <div>
+          <KnowledgeSlider id={i.toString()} />
+        </div>
+      </SwiperSlide>
+    );
+  }
+
   return (
-  
-    <div className="swiper">  
-    <SetLeft backNotVisible={0} back="slider" />
-    <Swiper
-      spaceBetween={30}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      autoplay={{ delay: 3000 }}
-    >
-      <SwiperSlide>
-        { <div>Slide 1 Content</div>}
-      </SwiperSlide>
-      <SwiperSlide>
-        { <div>Slide2 Content</div>}
-      </SwiperSlide>
-   
-    </Swiper></div>
+    <div className="swiper-container">
+      <SetLeft backNotVisible={0} back="slider" />
+      <SetRight />
+      <Swiper
+        spaceBetween={400}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {slides}
+      </Swiper>
+    </div>
   );
 };
 
